3c25ced3403874d0e824d7bf8101cb654d91a38a
Milan <dangi.milan46@gmail.com>
2024-11-21 21:09:07 +0545

    successfully integrating html in flask

--- main.py
+++ main.py
@@ -7,35 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def Welcome():
     return render_template('index.html')
-@app.route('/sucess/<int:score>')
-def sucess(score):
-   result =""
-   if score >= 50:
-      result = "pass"
-   else:
-      result = "fail"
-   return render_template('result.html',result =result)
    #return "Congratulations,You passed the exam"
-# @app.route('/fail/<int:score>')
-# def fail(score):
-#    return "You failed in exam, better for next time"
+@app.route('/success/<int:score>')
+def success(score):
+   return render_template('result.html',result='pass',score=score)
+@app.route('/fail/<int:score>')
+def fail(score):
+   return render_template('result.html',result='fail',score=score)
+
 ##Result checker
-@app.route('/submit',methods=['POST','Get'])
+@app.route('/submit',methods=['POST','GET'])
 def submit():
    total_score = 0
-   if request.method=='post':
-      science = float(request.form['Science'])
-      math = float(request.form['Math'])
-      Cprog = float(request.form['C programming'])
-      English = float(request.form['English'])
-      Nepali = float(request.form['Nepali'])
+   if request.method=='POST':
+      science = float(request.form['sc'])
+      math = float(request.form['mth'])
+      Cprog = float(request.form['cprog'])
+      English = float(request.form['Eng'])
+      Nepali = float(request.form['Nep'])
       total_score = (science + math + Cprog + English + Nepali)/5
-
-
-   result = ""
-   if total_score >= 50:
-      result = "sucess"
-   return redirect(url_for(result,score = total_score))
+   
+   if total_score >= 40:
+      return redirect(url_for('success',score=total_score))
+   
+   else:
+      return redirect(url_for('fail',score=total_score))
+      
+   
 
 if __name__=='__main__':
    app.run(debug=True)
